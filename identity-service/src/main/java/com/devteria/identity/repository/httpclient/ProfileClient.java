b12c3b6d97e366d3660ea09ee9313d343b0ef2a6
duongkaka <duongkaka96@gmail,com>
2025-10-27 21:35:31 +0900

    build api gateway by String clould Gateway

--- identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -11,6 +11,6 @@ import com.devteria.identity.dto.request.ProfileCreationRequest;
 @FeignClient(name = "profile-service", url = "${app.services.profile}")
 public interface ProfileClient {
 
-    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/internal/", produces = MediaType.APPLICATION_JSON_VALUE)
     UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request);
 }
