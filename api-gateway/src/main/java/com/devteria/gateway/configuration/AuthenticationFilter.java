e8a1e580310fa4fb5bbf45433d46c16eef2fe2a1
duongkaka <duongkaka96@gmail,com>
2025-10-31 16:14:46 +0900

    ouathtication add profile-service

--- api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
@@ -8,13 +8,16 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -24,6 +27,7 @@ import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServer;
 import reactor.netty.http.server.HttpServerResponse;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -33,16 +37,31 @@ import java.util.List;
 public class AuthenticationFilter implements GlobalFilter, Ordered {
     IdentityService identityService;
     ObjectMapper objectMapper;
+    @NonFinal
+    private String[]publicEndpoints = {"/identity/auth/.*","/identity/auth/token","/identity/auth/introspect","/identity/auth/logout","/identity/users/registration"};
+
+    @Value("${app.api-prefix}")
+    @NonFinal
+    private String apiPrefix;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("enter authentication filter");
+        log.info("enter authentication filter.....");
+        System.out.println(isPublicEndpoint(exchange.getRequest()));
+
+        if (isPublicEndpoint(exchange.getRequest()))
+            return chain.filter(exchange);
+
+
         // Get token from authorization header
        List<String>authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (CollectionUtils.isEmpty(authHeader))
             return unauthenticated(exchange.getResponse());
 
-        String token = authHeader.getFirst().replace("Bearer","");
+        String token = authHeader.getFirst();
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
         log.info("Token : {}",token);
         // verify token
         return identityService.introspect(token).flatMap(introspectResponseApiResponse -> {
@@ -55,10 +74,17 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
 
     }
 
+    private boolean isPublicEndpoint(org.springframework.http.server.reactive.ServerHttpRequest request) {
+        return Arrays.stream(publicEndpoints)
+                .anyMatch(endpoint -> request.getURI().getPath().startsWith(apiPrefix + endpoint));
+    }
+
     @Override
     public int getOrder() {
         return -1;
     }
+
+
 
     Mono<Void>unauthenticated(ServerHttpResponse response) {
         ApiResponse<?>apiResponse = ApiResponse.builder()
@@ -76,7 +102,7 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         }
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
 
-        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_CBOR_VALUE);
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
     return response.writeWith(
             Mono.just(response.bufferFactory().wrap(body.getBytes()))
