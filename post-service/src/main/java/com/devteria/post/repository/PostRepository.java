828b9ab6739ced5505d9c46621ba585d2ffc22b9
duongkaka <duongkaka96@gmail,com>
2025-11-09 08:58:21 +0900

    send mail by kafka

--- post-service/src/main/java/com/devteria/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devteria.post.repository;
-
-import com.devteria.post.entity.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-}
