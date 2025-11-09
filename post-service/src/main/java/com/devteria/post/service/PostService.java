0ca85eeefc809a40e1b0db2c34fc2f19d3baa272
duongkaka <duongkaka96@gmail,com>
2025-11-09 15:05:33 +0900

    Map/Dictionary-Based Strategy design pattern

--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -1,13 +1,12 @@
 package com.devteria.post.service;
 
-import com.devteria.post.dto.ApiResponse;
+
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PageResponse;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.entity.Post;
 import com.devteria.post.mapper.PostMapper;
 import com.devteria.post.repository.PostRepository;
-import com.nimbusds.jose.proc.SecurityContext;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -20,7 +19,7 @@ import org.springframework.stereotype.Service;
 
 
 import java.time.Instant;
-import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +27,7 @@ import java.util.List;
 public class PostService {
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeFormatter dateTimeFormatter;
 
 
     public PostResponse createPost(PostRequest request){
@@ -53,6 +53,11 @@ public class PostService {
         Pageable pageable = PageRequest.of(page -1,size,sort);
 
         var pageData = postRepository.findAllByUserId(userId,pageable);
+        var postList = pageData.getContent().stream().map( post ->{
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
 
 
         return PageResponse.<PostResponse>builder()
@@ -60,10 +65,8 @@ public class PostService {
                 .pageSize(pageData.getSize())
                 .totalPages(pageData.getTotalPages())
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .data(postList)
                 .build();
-
-
 
     }
 }
